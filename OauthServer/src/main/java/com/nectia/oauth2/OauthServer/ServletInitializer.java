a5e714fae7b11f288e711987ea6bc0e760bf8967
Mosheh-Efra Landaeta <mlandaeta@nectia.com>
2016-08-08 09:46:49 -0300

    Start: OAuthServer WAR

---
+++ OauthServer/src/main/java/com/nectia/oauth2/OauthServer/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.nectia.oauth2.OauthServer;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(OauthServerApplication.class);
+	}
+
+}
